992d99df5651db98aaff98adfa6826c6926c28fa
Munkhtulga <munkhtulga.battogtokh@gmail.com>
2023-02-18 15:41:29 +0000

    Implement basic importance widget

--- app.py
+++ app.py
@@ -1,35 +1,131 @@
-from flask import Flask, send_from_directory
+from curses import raw
+from flask import Flask, send_from_directory, request
+from numba import jit
+import numpy as np
+from scipy.special import softmax
+from importance import attention_importance, lime_importance, gradient_importance
 
-DEVICE = "cuda"
+
+DEVICE = "cpu"
 DATASET = "banking"
+if DATASET == "banking":
+    num_labels = 77
+elif DATASET == "hwu":
+    num_labels = 64
+elif DATASET == "clinc":
+    num_labels = 150
+
+@jit
+def inner_product_distance(a,b, tau=15):
+    return np.exp(-np.sum(a * b) / tau)**2
 
 
-def load_model():
-    from transformers import AutoModel, TrainingArguments, Trainer
+class TextProcessor:
+    def __init__(self, dataset, num_labels):
+        self.tau = 15
 
-    if DATASET == "banking":
-        num_labels = 77
-    elif DATASET == "hwu":
-        num_labels = 64
-    elif DATASET == "clinc":
-        num_labels = 150
+        # Load dataset for its metadata
+        from datasets import Dataset, DatasetDict
+        from sklearn import preprocessing
 
-    checkpoint = "./models/" + DATASET
+        data = {
+            "train": "/home/mojo/projects/phd/stage1/dialogue_tasks/DialoGLUE/data_utils/dialoglue/" + DATASET + "/train.csv",
+            "val": "/home/mojo/projects/phd/stage1/dialogue_tasks/DialoGLUE/data_utils/dialoglue/" + DATASET + "/val.csv",
+            "test": "/home/mojo/projects/phd/stage1/dialogue_tasks/DialoGLUE/data_utils/dialoglue/" + DATASET + "/test.csv",
+        }
+        raw_datasets = DatasetDict.from_csv({key: path for key, path in data.items()})
+        le = preprocessing.LabelEncoder()
+        le.fit(raw_datasets["train"]["category"] + raw_datasets["test"]["category"])
+        self.class_names = le.classes_
+        self.raw_datasets = raw_datasets
 
-    model = AutoModel.from_pretrained(checkpoint, num_labels=num_labels)
-    model.to(DEVICE)
+        import json
+        prediction_data_file = "static/data/" + dataset + "-viz_data-8-clusters-intent_cluster_chosen_by_majority_in-predicted-intent.json"
+        with open(prediction_data_file, "r") as f:
+            self.prediction_data = json.load(f)
 
-    return model
+        # Load model
+        from transformers import AutoModel, AutoTokenizer
+        checkpoint = "./models/" + dataset
 
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        self.model = AutoModel.from_pretrained(checkpoint, num_labels=num_labels)
+        self.model.to(DEVICE)
+
+        # A version of the same model for integrated gradients
+        from transformers import BertModel
+        class BertForEncoding(BertModel):
+            def forward(self, *args, **kwargs):
+                encoding = super().forward(*args, **kwargs).last_hidden_state[:,0]
+                output = encoding.sum(dim=-1)
+                return output
+            
+        self.model_for_ig = BertForEncoding.from_pretrained(checkpoint)
+
+    
+    def process(self, text):
+        encoding = self.encode(text)
+        return {"encoding": encoding.tolist()}
+
+    def encode(self, text):
+        tokenized = self.tokenizer(text, return_tensors="pt")
+        tokenized.to(DEVICE)
+        outputs = self.model(**tokenized)
+        return outputs.last_hidden_state.squeeze()[0].detach().cpu().numpy()
+    
+    def importance(self, index, method):
+        text = self.raw_datasets["test"]["text"][index]
+
+        if method == "attention":
+            importance, tokens = attention_importance(self.tokenizer, self.model, text)
+            return importance, tokens
+        elif method == "lime":
+            pred_data = self.prediction_data[index]
+            support_set_idxs = pred_data["support_set"]
+            support_set = self.raw_datasets["test"][support_set_idxs]
+            importance = lime_importance(self.tokenizer, self.model, text, support_set)
+            return importance
+        elif method == "gradient":
+            importance = gradient_importance(self.tokenizer, self.model_for_ig, text)
+            return importance
+
+    def importances_all(self, index):
+        attn_importance, tokens = self.importance(index, "attention")
+        lime_importance, tokens = self.importance(index, "lime")
+        grad_importance, tokens = self.importance(index, "gradient")
+
+        return {"tokens": tokens, 
+                "attn_importance": attn_importance,
+                "lime_importance": lime_importance,
+                "grad_importance": grad_importance}
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, static_url_path="/static/")
 
-    model = load_model()
+    text_processor = TextProcessor(DATASET, num_labels)
 
     @app.route("/")
     def main():
         return send_from_directory("./", "index.html")
+
+    @app.route("/process_text")
+    def process_text():
+        text = request.args.get("text")
+        result = text_processor.process(text)
+        return result
+
+    @app.route("/importance")
+    def importance():
+        index = int(request.args.get("index"))
+        method = request.args.get("method")
+        importance, tokens = text_processor.importance(index, method)
+        return {"importance": importance, "tokens": tokens}
+    
+    @app.route("/importances")
+    def importance_all():
+        index = int(request.args.get("index"))
+        result = text_processor.importances_all(index)
+        return result
 
     return app
---
+++ importance.py
@@ -0,0 +1,96 @@
+from lib2to3.pgen2.token import tok_name
+import torch
+import math
+
+
+def attention_importance(tokenizer, model, text, device="cuda"):
+    tokenized_input = tokenizer(text, return_tensors="pt")
+    tokenized_input.to(device)
+    model.to(device)
+    outputs = model(**tokenized_input, output_attentions=True)
+
+    import torch
+    attentions = torch.stack(outputs.attentions)
+    attentions_aggregated = attentions.squeeze().sum(0).sum(0).detach().cpu()
+    attentions_importance = attentions_aggregated.sum(0) / 144
+    tokens = tokenizer.convert_ids_to_tokens(tokenized_input["input_ids"][0])
+    tokens = tokens[1:-1]
+
+    importance = attentions_importance.tolist()[1:-1]
+    return importance, tokens
+
+
+def lime_importance(tokenizer, model, text, support_set, device="cuda"):
+
+    def one_sentence_tokenize(text):
+        tokens = tokenizer.tokenize(text)
+        return [token.replace("#", "") for token in tokens]
+
+    def encode(texts):
+        tokenized_xs = tokenizer.batch_encode_plus(texts, max_length=50, 
+                                                truncation=True, padding=True, return_tensors="pt")
+        tokenized_xs.to(device)
+        outputs = []
+        num_batches = math.ceil(len(texts) / BATCH_LIMIT)
+        
+        for batch_idx in range(num_batches):
+            tokenized_xs_batched = dict(
+                input_ids=tokenized_xs["input_ids"][batch_idx*BATCH_LIMIT: (batch_idx + 1) * BATCH_LIMIT].to(device),
+                token_type_ids=tokenized_xs["token_type_ids"][batch_idx*BATCH_LIMIT: (batch_idx + 1) * BATCH_LIMIT].to(device),
+                attention_mask=tokenized_xs["attention_mask"][batch_idx*BATCH_LIMIT: (batch_idx + 1) * BATCH_LIMIT].to(device),
+            )
+            
+            outputs_batched = model(**tokenized_xs_batched)
+            outputs.append( outputs_batched.last_hidden_state[:,0,:] )
+    
+        outputs = torch.cat(outputs)
+        return outputs
+
+    def classify(texts):
+        import torch
+        text_encoding = encode(texts)
+        similarities = torch.inner(text_encoding, support_encodings)
+        probs = torch.softmax(similarities/TAU, dim=-1)
+        return probs.detach().cpu().numpy()
+
+    from lime.lime_text import LimeTextExplainer
+
+    model.to(device)
+    BATCH_LIMIT = 2
+    TAU = 15
+
+    support_encodings = encode(support_set["text"])
+    probs = classify([text])
+    label = probs[0].argmax()
+
+    LIME_explainer_1sent = LimeTextExplainer(
+                        class_names=support_set["category"], 
+                        bow=False, 
+                        split_expression=one_sentence_tokenize, 
+                        mask_string=tokenizer.mask_token)
+    exp = LIME_explainer_1sent.explain_instance(text,
+                        classify, top_labels=5, num_samples=100
+                        , num_features=100)
+    
+    tokens = one_sentence_tokenize(text)
+    importance = sorted(exp.as_map()[label], key=lambda x: x[0])
+    importance = [weight for pos, weight in importance]
+    return importance, tokens
+
+
+def gradient_importance(tokenizer, model, text, device="cuda"):
+    from captum.attr import LayerIntegratedGradients
+
+    lig = LayerIntegratedGradients(model, model.embeddings)
+    tokenized_inputs = tokenizer(text, return_tensors="pt")
+    input = tokenized_inputs["input_ids"]
+
+    attributions_ig, delta = lig.attribute(
+        input, 
+        return_convergence_delta=True,
+        attribute_to_layer_input=False
+    )
+    tokens = tokenizer.tokenize(text)
+    importance = attributions_ig.sum(-1).squeeze().tolist()[1:-1]
+    print(importance)
+    return importance, tokens
